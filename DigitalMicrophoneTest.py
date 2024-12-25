1512e6c41af1f946ddfcb58f517c0c5bd7b45679
omeletflipper <82295971+omeletflipper@users.noreply.github.com>
2024-12-25 16:36:14 -0600

    Update DigitalMicrophoneTest.py

--- DigitalMicrophoneTest.py
+++ DigitalMicrophoneTest.py
@@ -7,7 +7,6 @@ GPIO.setup(MIC_PIN, GPIO.IN)
 
 try:
     while True:
-        time.sleep(0.1)
         if GPIO.input(MIC_PIN) == GPIO.HIGH:  # Sound detected
             print("Sound detected")
         else:
