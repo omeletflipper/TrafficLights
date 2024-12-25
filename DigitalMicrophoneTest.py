06716279add79ecae3682d67004cdb80f3d52446
omeletflipper <82295971+omeletflipper@users.noreply.github.com>
2024-12-25 16:27:52 -0600

    Update DigitalMicrophoneTest.py

--- DigitalMicrophoneTest.py
+++ DigitalMicrophoneTest.py
@@ -7,9 +7,12 @@ GPIO.setup(MIC_PIN, GPIO.IN)
 
 try:
     while True:
-        if GPIO.input(MIC_PIN) == GPIO.HIGH:  # Sound detected
-            print("Sound detected!")
         time.sleep(0.1)
+        if GPIO.input(MIC_PIN) == GPIO.HIGH:  # Sound detected
+            print("Sound detected")
+        else:
+            print("No sound)
+
 
 except KeyboardInterrupt:
     GPIO.cleanup()
