00584c2d6e6a8ab7ef375839e8cc7931e7645441
omeletflipper <82295971+omeletflipper@users.noreply.github.com>
2024-12-31 17:20:26 -0600

    Update ShiftRegisterTest.py

--- ShiftRegisterTest.py
+++ ShiftRegisterTest.py
@@ -27,10 +27,10 @@ def latch_data():
 # Turn on all 16 LEDs (binary 1111111111111111)
 try:
     while True:
-        shift_out(0b1111111111111111)  # 0xFFFF = 1111111111111111 in binary (turn on all 16 LEDs)
+        shift_out(0b11)  # 0xFFFF = 1111111111111111 in binary (turn on all 16 LEDs)
         latch_data()  # Update the LEDs
         time.sleep(1)  # Keep LEDs on for 1 second
-        shift_out(0b0000000000000000)
+        shift_out(0b00)
         latch_data()
         time.sleep(1)
 except KeyboardInterrupt:
