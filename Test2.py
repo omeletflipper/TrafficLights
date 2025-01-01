02f3cd21059fd2ad3c1b1195fd41ebde0f04d120
omeletflipper <82295971+omeletflipper@users.noreply.github.com>
2025-01-01 17:38:35 -0600

    Update and rename Test2 to Test2.py

---
+++ Test2.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+DATA_PIN = 16
+CLOCK_PIN = 21
+LATCH_PIN = 20
+
+GPIO.setup(DATA_PIN, GPIO.OUT)
+GPIO.setup(CLOCK_PIN, GPIO.OUT)
+GPIO.setup(LATCH_PIN, GPIO.OUT)
+
+def setOutput(ledpattern)
+    GPIO.output(LATCH_PIN, 0)
+    for x in range(8):
+        GPIO.output(DATA_PIN, int (ledpattern[x - 1]))
+        GPIO.output(CLOCK_PIN, 1)
+        GPIO.output(CLOCK_PIN, 0)
+    GPIO.out(LATCH_PIN, 1)
+
+try:
+    while True:
+        for i in range(5)
+            setOutput("10010010")
+            time.sleep(0.5)
+            setOutput("01001001")
+            time.sleep(0.5)
+            setOutput("00100100")
+        time.sleep(0.5)
+        setOutput("11111111")
