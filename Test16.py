6881a9314474153963678877cd4d17e38bc74624
omeletflipper <82295971+omeletflipper@users.noreply.github.com>
2025-01-01 18:45:30 -0600

    Update Test16.py

--- Test16.py
+++ Test16.py
@@ -23,24 +23,23 @@ def setOutput(ledpattern):
 
 try:
     while True:
-        setOutput("1000000000000000")  
+        setOutput("0000000010000000")
         time.sleep(0.5)
-        setOutput("0100000000000000")
+        setOutput("0000000001000000")
         time.sleep(0.5)
-        setOutput("0010000000000000")
+        setOutput("0000000000100000")
         time.sleep(0.5)
-        setOutput("0001000000000000")
+        setOutput("0000000000010000")
         time.sleep(0.5)
-        setOutput("0000100000000000")
+        setOutput("0000000000001000")
         time.sleep(0.5)
-        setOutput("0000010000000000")
+        setOutput("0000000000000100")
         time.sleep(0.5)
-        setOutput("0000001000000000")
+        setOutput("0000000000000010")
         time.sleep(0.5)
-        setOutput("0000000100000000")
+        setOutput("0000000000000001")
         time.sleep(0.5)
-        # All LEDs on
-        setOutput("1111111111111111")  
+        setOutput("11111111") #all on
         time.sleep(0.5)
 
 except KeyboardInterrupt:
