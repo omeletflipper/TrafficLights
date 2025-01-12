506a358a8f82e914474f479852873736748b2d90
omeletflipper <82295971+omeletflipper@users.noreply.github.com>
2025-01-12 17:39:36 -0600

    Update distance.py

--- distance.py
+++ distance.py
@@ -4,44 +4,64 @@ import time
 import sys
 import signal
 
-def signal_handler(signal, frame): # ctrl + c -> exit program
-        print('You pressed Ctrl+C!')
-        sys.exit(0)
 signal.signal(signal.SIGINT, signal_handler)
-
 gpio.setmode(gpio.BCM)
-trig = 23 # 7th
-echo = 18 # 6th
 
-gpio.setup(trig, gpio.OUT)
-gpio.setup(echo, gpio.IN)
+#Sensor 1
+trig1 = 23
+echo1 = 18
+
+#Sensor 2
+trig2 = 20
+echo2 = 21
+
+# Set up GPIO
+gpio.setup(trig1, gpio.OUT)
+gpio.setup(echo1, gpio.IN)
+gpio.setup(trig2, gpio.OUT)
+gpio.setup(echo2, gpio.IN)
 
 time.sleep(0.5)
-print ('-----------------------------------------------------------------sonar start')
-try :
-    while True :
-        gpio.output(trig, False)
-        time.sleep(0.1)
-        gpio.output(trig, True)
-        time.sleep(0.00001)
-        gpio.output(trig, False)
-        while gpio.input(echo) == 0 :
-            pulse_start = time.time()
-        while gpio.input(echo) == 1 :
-            pulse_end = time.time()
-        pulse_duration = pulse_end - pulse_start
-        distance = pulse_duration * 17000
-        if pulse_duration >=0.01746:
-            print('time out')
-            continue
-        elif distance > 300 or distance==0:
-            print('out of range')
-            continue
-        distance = round(distance, 3)
-        print ('Distance : %f cm'%distance)
+print('-----------------------------------------------------------------sonar start')
+
+# Function to measure distance
+def measure_distance(trig, echo):
+    gpio.output(trig, False)
+    time.sleep(0.1)
+    gpio.output(trig, True)
+    time.sleep(0.00001)
+    gpio.output(trig, False)
+
+    while gpio.input(echo) == 0:
+        pulse_start = time.time()
+    while gpio.input(echo) == 1:
+        pulse_end = time.time()
+
+    pulse_duration = pulse_end - pulse_start
+    distance = pulse_duration * 17000
+
+    # Timeout and range validation
+    if pulse_duration >= 0.01746:
+        return 'timeout'
+    elif distance > 300 or distance == 0:
+        return 'out of range'
+
+    return round(distance, 3)
+
+# Main loop
+try:
+    while True:
+        # Measure distance for Sensor 1
+        distance1 = measure_distance(trig1, echo1)
+        print('Sensor 1 Distance: {}'.format(distance1))
+
+        # Measure distance for Sensor 2
+        distance2 = measure_distance(trig2, echo2)
+        print('Sensor 2 Distance: {}'.format(distance2))
 
 except (KeyboardInterrupt, SystemExit):
     gpio.cleanup()
     sys.exit(0)
-except:
+except Exception as e:
+    print(f"An error occurred: {e}")
     gpio.cleanup()
